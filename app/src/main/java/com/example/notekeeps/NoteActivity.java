1e24f7c26dd76baf710d3398e7ed50b91fd613c1
lifeofdan_el <anomfuemedaniel@gmail.com>
2019-08-31 11:55:05 +0100

    Intial commit

--- app/src/main/java/com/example/notekeeps/NoteActivity.java
+++ app/src/main/java/com/example/notekeeps/NoteActivity.java
@@ -18,6 +18,10 @@ import java.util.List;
 public class NoteActivity extends AppCompatActivity {
 
     public static final String NOTE_POSITION = "com.example.notekeeps.NOTE_POSITION";
+    public static final String ORIGINAL_NOTE_COURSE_ID = "com.example.notekeeps.ORIGINAL_NOTE_COURSE_ID";
+    public static final String ORIGINAL_NOTE_TITLE = "com.example.notekeeps.ORIGINAL_NOTE_TITLE ";
+    public static final String ORIGINAL_NOTE_TEXT = "com.example.notekeeps.ORIGINAL_NOTE_TEXT ";
+
     public static final int POSITION_NOT_SET = -1;
     private NoteInfo mNote;
     private boolean mIsNewNote;
@@ -26,6 +30,9 @@ public class NoteActivity extends AppCompatActivity {
     private EditText mTextNoteText;
     private int mNotePosition;
     private boolean mIsCancelling;
+    private String mOriginalNoteCourseId;
+    private String mOriginalNoteTitle;
+    private String mOriginalNoteText;
 
 
     @Override
@@ -45,12 +52,21 @@ public class NoteActivity extends AppCompatActivity {
 
 
         readDisplayStateValues();
+        saveOriginalNoteValues();
 
         mTextNoteTitle =  findViewById(R.id.text_note_title);
         mTextNoteText = findViewById(R.id.text_note_text);
 
         if (!mIsNewNote)
              displayNote(mSpinnerCourses, mTextNoteTitle, mTextNoteText);
+    }
+
+    private void saveOriginalNoteValues() {
+        if (mIsNewNote)
+            return;
+        mOriginalNoteCourseId = mNote.getCourse().getCourseId();
+        mOriginalNoteTitle = mNote.getTitle();
+        mOriginalNoteText = mNote.getText();
     }
 
     private void displayNote(Spinner spinnerCourses, EditText textNoteTitle, EditText textNoteText) {
@@ -70,10 +86,19 @@ public class NoteActivity extends AppCompatActivity {
         if (mIsCancelling){
             if (mIsNewNote) {
                 DataManager.getInstance().removeNote(mNotePosition);
+            } else{
+                storePreviousNoteValues();
             }
         } else {
             saveNote();
         }
+    }
+
+    private void storePreviousNoteValues() {
+        CourseInfo course = DataManager.getInstance().getCourse(mOriginalNoteCourseId);
+        mNote.setCourse(course);
+        mNote.setTitle(mOriginalNoteTitle);
+        mNote.setText(mOriginalNoteText);
     }
 
     private void saveNote() {
