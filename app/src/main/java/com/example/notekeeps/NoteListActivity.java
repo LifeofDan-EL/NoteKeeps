4ed95ed06887f4aab6b7b31e8fa6b89dae39552e
lifeofdan_el <anomfuemedaniel@gmail.com>
2019-11-13 21:12:47 +0100

    Intial commit

--- app/src/main/java/com/example/notekeeps/NoteListActivity.java
+++ app/src/main/java/com/example/notekeeps/NoteListActivity.java
@@ -18,8 +18,7 @@ import android.widget.ListView;
 import java.util.List;
 
 public class NoteListActivity extends AppCompatActivity {
-
-   // private ArrayAdapter<NoteInfo> mAdapterNotes;
+    private NoteRecyclerAdapter mNoteRecyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,37 +36,21 @@ public class NoteListActivity extends AppCompatActivity {
         });
 
         initializeDisplayContent();
-
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-      //  mAdapterNotes.notifyDataSetChanged();
+       mNoteRecyclerAdapter.notifyDataSetChanged();
     }
 
     private void initializeDisplayContent() {
-   //    final  ListView listNotes = findViewById(R.id.list_notes);
-//
-  //      List<NoteInfo> notes = DataManager.getInstance().getNotes();
-    //    mAdapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
-
-      //  listNotes.setAdapter(mAdapterNotes);
-
-      //  listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-       //     @Override
-         //   public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-            //    Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
-//                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
-             //   intent.putExtra(NoteActivity.NOTE_POSITION, position);
-
-               // startActivity(intent);
-
-       //     }
-        //})  ;
         final RecyclerView recyclerNotes = findViewById(R.id.list_notes);
         final LinearLayoutManager notesLayoutManager = new LinearLayoutManager(this);
         recyclerNotes.setLayoutManager(notesLayoutManager);
 
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        mNoteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
+        recyclerNotes.setAdapter(mNoteRecyclerAdapter);
     }
 }
--- app/src/main/java/com/example/notekeeps/NoteRecyclerAdapter.java
+++ app/src/main/java/com/example/notekeeps/NoteRecyclerAdapter.java
@@ -1,21 +1,25 @@
 package com.example.notekeeps;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder> {
 
     private final Context mContext;
+    private final List<NoteInfo> mNotes;
     private final LayoutInflater mLayoutInflater;
 
-    public NoteRecyclerAdapter(Context context) {
+    public NoteRecyclerAdapter(Context context, List<NoteInfo> notes) {
         mContext = context;
+        mNotes = notes;
         mLayoutInflater = LayoutInflater.from(mContext);
     }
 
@@ -28,12 +32,15 @@ public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapte
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        NoteInfo note = mNotes.get(position);
+        holder.mTextCourse.setText(note.getCourse().getTitle());
+        holder.mTextTitle.setText(note.getTitle());
+        holder.mCurrentPosition = position;
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return  mNotes.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -41,11 +48,21 @@ public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapte
 
         public final TextView mTextCourse;
         public final TextView mTextTitle;
+        public int mCurrentPosition;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             mTextCourse = itemView.findViewById(R.id.text_course);
             mTextTitle = itemView.findViewById(R.id.text_title);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(mContext, NoteActivity.class);
+                    intent.putExtra(NoteActivity.NOTE_POSITION, mCurrentPosition);
+                    mContext.startActivity(intent);
+                }
+            });
         }
     }
 }
