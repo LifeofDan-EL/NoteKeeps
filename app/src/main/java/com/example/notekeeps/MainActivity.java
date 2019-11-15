ee6aad2659d7be909ff179c084b87f11ef812a82
lifeofdan_el <anomfuemedaniel@gmail.com>
2019-11-15 16:30:05 +0100

    Intial commit

--- app/src/main/java/com/example/notekeeps/MainActivity.java
+++ app/src/main/java/com/example/notekeeps/MainActivity.java
@@ -155,9 +155,9 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_courses){
             displayCourses();
         } else if (id == R.id.nav_share){
-            handleSelection("Share");
+            handleSelection(R.string.nav_share_message);
         } else if (id == R.id.nav_send){
-            handleSelection("Send");
+            handleSelection(R.string.nav_send_message);
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -165,9 +165,9 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    private void handleSelection(String message) {
+    private void handleSelection(int  message_id) {
         View view = findViewById(R.id.list_items);
-        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
+        Snackbar.make(view, message_id, Snackbar.LENGTH_LONG).show();
 
     }
 }
